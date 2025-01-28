3f27ca7dc69033619f41757a8793b11a67c937eb
Florian. M <florian.m@v38armageddon.net>
2025-01-28 14:30:28 +0000

    [klpctl] Replace C++ project => C#

---
+++ src/KLP.CLI/Commands/InstallCommand.cs
@@ -0,0 +1,3 @@
+using System;
+using System.CommandLine;
+using System.Diagnostics;
---
+++ src/KLP.CLI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.CommandLine;
+
+var rootCommand = new RootCommand("klpctl -  CLI Interface for Kernel LivePatch ");
+var installCommand = new Command("install", "Install a KLP patch.")
+{
+    new Argument<string>("patch", "The patch to install (in .klp format).")
+}
+installCommand.SetHandler((string file) =>
+{
+    InstallPatch(file);
+}, installCommand.Arguments[0]);
+var uninstallCommand = new Command("uninstall", "Uninstall a KLP patch.")
+{
+    new Argument<string>("patch", "The patch to uninstall.")
+} 
+uninstallCommand.SetHandler((string patch) =>
+{
+    UninstallPatch(patch);
+}, uninstallCommand.Arguments[0]);
+
+rootCommand.AddCommand(new Command("list", "List all installed KLP patches."));
+rootCommand.AddCommand(installCommand);
+rootCommand.AddCommand(uninstallCommand);
+rootCommand.AddCommand(new Command("update", "Update the KLP database and apply new patches."));
+
+return await rootCommand.InvokeAsync(args);
